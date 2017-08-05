494ab2e5dfe2e52f381554245607d9da255e127e
Erica Okamura <erica.okamura@yahoo.com.br>
2017-08-05 16:06:08 -0300

    ex. 3.2

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,6 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 
@@ -23,14 +24,17 @@ public class Sessao {
 	@ManyToOne
 	private Filme filme;
 	
+	private BigDecimal preco;
+	
 	public Sessao() {
 		
 	}
 	
-	public Sessao(LocalTime Horario, Filme filme, Sala sala) {
+	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.horario = horario;
 		this.setFilme(filme);
 		this.sala = sala;
+		this.preco = sala.getPreco().add(filme.getPreco());
 	}
 	
 	public Sala getSala() {
@@ -60,6 +64,14 @@ public class Sessao {
 	public Filme getFilme() {
 		return filme;
 	}
+	
+	public BigDecimal getPreco(){
+    	return preco;
+    }
+    
+    public void setPreco(BigDecimal preco){
+    	this.preco = preco;
+    }
 	
 	public void setFilme(Filme filme) {
 		this.filme = filme;
